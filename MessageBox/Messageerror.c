00fa3bc03c3581d4e2ae467e4f3f062276acb1b1
JsonChenJianShun <JsonChenJianShun@outlook.com>
2025-07-14 02:09:39 +0800

    add MessageBox

---
+++ MessageBox/Messageerror.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, "Error", "Error", MB_OK | MB_ICONERROR);
+    return 0;
+}
